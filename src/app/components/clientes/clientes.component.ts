186c2405a132b60ce6f95da8c226e98eb46457e6
38061027 <joseaugustogomessantos12@gmail.com>
2024-07-11 16:40:43 -0300

    usando o charjs

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.scss']
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit{
+  displayedColumns: string[] = ['id', 'name', 'telefone'];
+  clientes!:any[]
+
+  constructor(private service:SharedService){}
+
+  ngOnInit(): void {
+    this.getClientes()
+  }
+
+  getClientes(){
+    this.service.getClients().subscribe(res => this.clientes = res)
+  }
 
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart,registerables } from 'chart.js';
 import { SharedService } from 'src/app/services/shared.service';
 
 export interface PeriodicElement {
@@ -21,12 +22,20 @@ export class DashboardComponent implements OnInit {
   numeroClientes: number = 0
   numeroProdutos: number = 0
 
+  @ViewChild("meuCanvas", { static: true }) element!: ElementRef
+
+  vendas: any[] = [];
+  chart!: Chart
+
 
   constructor(private service: SharedService) { }
 
   ngOnInit(): void {
     this.getProdutos()
     this.service.getClients().subscribe(res => this.numeroClientes = res.length)
+    Chart.register(...registerables);
+    this.getVendas()
+    this.initializeChart()
 
   }
 
@@ -45,6 +54,68 @@ export class DashboardComponent implements OnInit {
 
       })
     })
+  }
+
+
+  initializeChart(): void {
+    this.chart = new Chart(this.element.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Vendas',
+            data: [],
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Produtos',
+              color: 'rgba(0, 0, 0, 1)'
+            },
+            ticks: {
+              color: 'rgba(0, 0, 0, 1)'
+            }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Quantidade',
+              color: 'rgba(0, 0, 0, 1)'
+            },
+            ticks: {
+              color: 'rgba(0, 0, 0, 1)'
+            }
+          }
+        },
+        
+      }
+    });
+  }
+
+
+  getVendas(): void {
+    this.service.getVendas().subscribe(res => {
+      this.vendas = res.slice(0, 5);
+      this.updateChart();
+    });
+  }
+
+
+  updateChart() {
+    this.chart.data.labels = this.vendas.map(el => el.produto)
+    this.chart.data.datasets[0].data = this.vendas.map(el => el.qtd)
+    this.chart.update();
   }
 
 
